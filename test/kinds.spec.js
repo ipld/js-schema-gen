4e7602ace044689c41d9a8e512a887c6bd194bed
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-08-22 04:56:00 +0000

    fix: kind tests and working null

---
+++ test/kinds.spec.js
@@ -0,0 +1,41 @@
+'use strict'
+const assert = require('assert')
+const { it } = require('mocha')
+const main = require('../')
+const parse = require('./parse')
+const tcompare = require('tcompare')
+const bytes = require('bytesish')
+
+const test = it
+
+const strict = (x, y) => assert.ok(tcompare.strict(x, y).match)
+
+test('all kinds in struct', done => {
+  const schema = `
+  type Test struct {
+    string String
+    int Int
+    float Float
+    bytes Bytes
+    map Map
+    list List
+    null Null
+  }
+  `
+  const classes = main(parse(schema))
+  const hw = {
+    string: 'test',
+    int: 1,
+    float: 1.1,
+    bytes: bytes.native('test'),
+    map: { hello: 'world' },
+    list: [null],
+    null: null
+  }
+  const t = new classes.Test(hw)
+
+  strict(t.encode(), hw)
+
+  strict(t.encode(), classes.Test.from(hw).encode())
+  done()
+})
